b4b8f4e41ba171409315344ebe8934e63a0ca265
gkibria121 <gkibria121@gmail.com>
2025-04-11 19:40:54 +0600

    update model and add create product functionality

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import ProductModel from "../models/ProductModel";
 
-export function index(req: Request, res: Response) {
+export async function index(req: Request, res: Response) {
+  const products = await ProductModel.find();
   res.status(200).send({
-    products: [],
+    products,
   });
+}
+
+export async function createProduct(req: Request, res: Response) {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(442).send({ message: "Name is required" });
+  }
+  const newProduct = await ProductModel.create({ name });
+  res.status(201).send({ product: newProduct });
 }
