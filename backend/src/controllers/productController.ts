e05b000a7a29e1f9e5ed8b9733cbfa02a6b8fa6a
gkibria121 <gkibria121@gmail.com>
2025-04-11 19:56:45 +0600

    bulk upload products

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import ProductModel from "../models/ProductModel";
+import { MongooseError } from "mongoose";
 
 export async function index(req: Request, res: Response) {
   const products = await ProductModel.find();
@@ -11,8 +12,32 @@ export async function index(req: Request, res: Response) {
 export async function createProduct(req: Request, res: Response) {
   const { name } = req.body;
   if (!name) {
-    return res.status(442).send({ message: "Name is required" });
+    res.status(442).send({ message: "Name is required" });
+    return;
   }
   const newProduct = await ProductModel.create({ name });
   res.status(201).send({ product: newProduct });
+}
+
+export async function bulkInsertOrUpdate(req: Request, res: Response) {
+  const { products } = req.body;
+  // Validate input
+  if (!Array.isArray(products) || products.length === 0) {
+    res.status(442).send({ message: "Missing or invalid fields" });
+    return;
+  }
+  // Insert products in bulk
+  try {
+    const newProducts = await ProductModel.insertMany(products);
+    res.status(201).send({
+      message: "Products added or updated successfully",
+      products: newProducts,
+    });
+  } catch (error) {
+    console.error("❌ Error:", error);
+    if (error instanceof MongooseError)
+      res.status(442).send({ message: "Validation failed" });
+    res.status(500).send({ message: "Internal server error" });
+    return;
+  }
 }
--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { index } from "../controllers/productController";
+import {
+  bulkInsertOrUpdate,
+  createProduct,
+  index,
+} from "../controllers/productController";
 
 const router = express.Router();
 
 router.route("/products").get(index);
+router.route("/products").post(createProduct);
+router.route("/products/bulk-upload").post(bulkInsertOrUpdate);
 
 export default router;
