dc7025be02f8ece40efb7dbc10007c5ff7ef7f52
gkibria121 <gkibria121@gmail.com>
2025-05-17 11:34:27 +0600

    add k8s

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,7 @@ import calculatePriceWithVendor from "./routes/calculationRoute2";
 import deliveryMethodRoute from "./routes/deliveryMethodRoute";
 import cors from "cors";
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/", product);
